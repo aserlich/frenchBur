31f038836ab014934caf028a47be8794ff0f5e4f
Aaron Erlich <aaron.erlich@gmail.com>
2012-08-12 17:46:01 -0700

    No only returns dictionary of entries

--- getPN.py
+++ getPN.py
@@ -10,9 +10,9 @@ import sys
 import os
 
 def getPN(fileName):
-	"""
+	"""pn
 	Finds the footers for each page of a book and returns the page number
-	Currently implemented for 2011
+	Currently implemented for 2011.
 	"""
 	fh = open(fileName)
 	fh.seek(0)
@@ -22,21 +22,30 @@ def getPN(fileName):
 	#pnE = regex.compile(r'', flag = regex.UNICODE)
 	pns =  []
 	locs = []
+	PNentries = []
 	for i in range(0, len(x)):
 		m = regex.search(pnO, x[i]) 
 		n = regex.search(pnE, x[i]) 
 		if m != None:
-			print(int(m.group(1)))
+			#print(int(m.group(1)))
 			pns.append(int(m.group(1)))
 			locs.append(i)
+			ref={}
+			ref['pageNum'] = int(m.group(1))
+			ref['loc'] = i
+			PNentries.append(ref)	
 		elif n!= None:
 			pns.append(int(n.group(1)))
-			print(n.group(1))
+			#print(n.group(1))
 			locs.append(i)
-	return([pns, locs])
+			ref={}
+			ref['pageNum'] = int(n.group(1))
+			ref['loc'] = i
+			PNentries.append(ref)	
+	return(PNentries)
 
 #To implement later are tests
-#pns = getPN('/Volumes/Optibay-1TB/FrenchBur/2011/rawText/gouv2011.003V1.3.txt')	
+pns = getPN('/Volumes/Optibay-1TB/FrenchBur/2011/rawText/gouv2011.003V1.3.txt')	
 #set(range(64,413)) -set(pns)
 #set(pns) - set(range(64,413))	
 #{304, 234, 93, 94}	
