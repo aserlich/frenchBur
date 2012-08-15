096205ac673209792de43af2860f3887535a3c67
Aaron Erlich <aaron.erlich@gmail.com>
2012-08-15 00:58:55 -0700

    A guide to current use of the functions

--- testCode.py
+++ testCode.py
@@ -6,27 +6,34 @@ Demonstration code for a suite functions to parse bibliographic data from books
 import os
 path = str('/Volumes/Optibay-1TB/FrenchBur/2011/rawText')
 
-# the fist step is to get a s set of dictionaries with the contents of each page
-#- This is done with getPages()
+#1
+# The fist step is to get a set of dictionaries with the contents of each page
+#-Done with getPages()
 pnEntries = getPages(path + 'gouv2011.003V3.0.txt')	
 
-#the next step is to get a set of index entries
-#this is from the table of contents, which currently returns a dictionary with two levels
-#it can be done from the index as well
+#2
+#The next step is to get a set of index entries
+#Entries could be from the table of contents, which currently returns a dictionary with two levels
+#It can be done from the index as well
 toc = getLevelsTOC(path + "toc2011.001V1.2.txt")
 lev1 = toc['lev1']
 lev2 = toc['lev2']
 
-#the next step is to find the bureacratic organizations names in the text
+#3
+#The next step is to find the bureacratic organizations' names in the text
 #this is done with matchBurLev()
 #this can be done recursively for each level of the bureacracy but I have done it with the highest right now
 burEntries = matchBurLev(pnEntries, lev1)
 
-#the next step is to get all of the text associated with that bureaucratic level. 
+#4
+#The next step is to get all of the text associated with apprpriate bureaucratic organization.
+#Eventually this will be need to be called recursively if organizations are nested.  
 burEntries = parseOrg(pnEntries, burEntries)
 burEntries[0] #yields the president
 
+#5
 #once all of the levels o bureacracy is matched - the remaining text will be parsed and old functions will be refactored for this
+#to yield appropriate fields for appropriate levels. 
 # demonstration of text to output csv
 # op2['entries']
 # keys = ['loc','name', 'nameFlag', 'rank','email','tel','fax', 'title']
